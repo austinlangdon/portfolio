cdb97c482e76f23890a5d00c0233810e858bac0a
austinlangdon <alangdon93@gmail.com>
2015-03-22 10:28:51 -0700

    initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,9 @@
+console.log('Run siteInfo() if you would like to find out more.');
+
+function siteInfo(){
+	console.log('This site was developed with Bootstrap, Sass, and charm.');
+}
+
+$('.connect').click(function(){
+	console.log('hi');
+});
